0e1ec39f610d616809285673feb691f5cd9833b4
Manuela Gutierrez Benitez <manuela.gutierrez2@utp.edu.co>
2024-11-14 22:47:38 -0500

    Update ui_mainwindow.cpp

--- Reproductor/ui_mainwindow.cpp
+++ Reproductor/ui_mainwindow.cpp
@@ -1,3 +1,5 @@
-#include "ui_mainwindow.h"
+#include "ui_mainwindow.h"    // Incluye el archivo de cabecera "ui_mainwindow.h", el cual contiene la declaración de la clase `ui_mainwindow`. 
+                            // Esta cabecera es generada automáticamente por Qt para gestionar los elementos de la interfaz de usuario definidos en `mainwindow.ui`.
 
-ui_mainwindow::ui_mainwindow() {}
+ui_mainwindow::ui_mainwindow() {}  // Constructor de la clase `ui_mainwindow`, creado automáticamente.  En este caso, está vacío, ya que no se requiere ninguna inicialización específica.
+                                   // Este constructor se ejecuta cuando se instancia un objeto `ui_mainwindow`,  permitiendo que los elementos de la interfaz estén disponibles en la clase `mainwindow`.
